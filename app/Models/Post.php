3dfe5651d8a228d108fb2ba144a84e5e9ee92f2b
Faideksan <faideksan.oniandini@gmail.com>
2024-11-11 19:56:52 +0700

    Membuat 5 artikel di database sqllite-default

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,12 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Arr;
 
-
-class Post extends Model // Terhubung ke tabel blog_posts
-
+class Post extends Model
 {
     protected $fillable = ['title', 'author', 'slug', 'body'];
+    use HasFactory;
 }
